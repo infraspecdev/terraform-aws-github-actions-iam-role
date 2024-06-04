4a2500596efbf0cc90e23389a07414c38b9d31ac
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2024-06-04 12:16:42 +0530

    github oidc connect resource added

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+resource "aws_iam_openid_connect_provider" "github_oidc_connect" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+}
 resource "aws_iam_role" "github_actions_role" {
   name               = var.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
